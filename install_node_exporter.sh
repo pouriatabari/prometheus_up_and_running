a3295c35f235ee29ad1f9366e53d280bfe20353f
Pouriya Mashali Tabari <95964285+pouriatabari@users.noreply.github.com>
2023-01-20 15:37:59 +0330

    Create install_node_exporter.sh

---
+++ install_node_exporter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+useradd -M -r -s /bin/false node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar -xvzf node_exporter-1.5.0.linux-amd64.tar.gz
+cp node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin/
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+cp /services/node_exporter.service /etc/systemd/system/node_exporter.service
