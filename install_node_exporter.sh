a5bbb5282a26b3bf3976e752173e53b69a118a8e
Pouriya Mashali Tabari <95964285+pouriatabari@users.noreply.github.com>
2023-01-20 15:51:29 +0330

    Update install_node_exporter.sh

--- install_node_exporter.sh
+++ install_node_exporter.sh
@@ -5,3 +5,9 @@ tar -xvzf node_exporter-1.5.0.linux-amd64.tar.gz
 cp node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin/
 chown node_exporter:node_exporter /usr/local/bin/node_exporter
 cp /services/node_exporter.service /etc/systemd/system/node_exporter.service
+systemctl daemon-reload
+systemctl start node_exporter
+systemctl enable node_exporter
+#curl localhost:9100/metrics
+firewall-cmd --add-port=9100/tcp --permanent
+firewall-cmd --reload
